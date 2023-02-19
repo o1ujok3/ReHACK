d272f52f81da55968f8313da680c5e30a1ac76f9
OzgunEfe <ozgunefe.dev@gmail.com>
2023-02-19 12:17:00 +0000

    New style added

---
+++ src/component/Title.js
@@ -0,0 +1,10 @@
+function Title(){
+    return(
+        <div className="title">
+            <h1>ReHack Recruitment</h1>
+            <h3>Where You Find You Dream Job</h3>
+        </div>
+    )
+}
+
+export default Title;
